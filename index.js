6640148a14b3abd7a834d20d45dba6b681bf2df0
Omar <59209539+OmarIhsan@users.noreply.github.com>
2025-02-21 11:05:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '6881296782:AAFDbdEWIkxE1uCaND64LxqOk-mAbFuNZQ0';
+const token = 'PASTE YOUR TOKEN HERE';
 
 // Create a bot instance
 const bot = new TelegramBot(token, { polling: true });
